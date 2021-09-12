00f850125fa99633eddc972b96ef4ef26512548b
WytheHuang <wythe.huangw@gmail.com>
2021-09-13 00:13:51 +0800

    mdf:refactor# 新增优雅关停gin服务

---
+++ cmd/app/utils/httpServe.go
@@ -0,0 +1,82 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+)
+// StartGinServ 启动 gin 服务
+func StartGinServ(isDefault bool, middleware ...gin.HandlerFunc) {
+	var r *gin.Engine
+	if isDefault {
+		r = gin.Default()
+	} else {
+		r = gin.New()
+	}
+	r.Use(middleware...)
+	// 初始化路由
+	parseRoute(r)
+	// 优雅关停服务
+	gracefulShutdown(r)
+}
+
+// parseRoute 解析路由
+func parseRoute(r *gin.Engine) {
+	// 解析路由定义
+	for group, routes := range RouterGroup {
+		groupRouter := r.Group(group)
+		for _, route := range routes {
+			// 是文件路由构建文件路由
+			if route.GetFilePath() != "" {
+				switch route.GetMethod() {
+				case "Static":
+					groupRouter.Static(route.GetURL(), route.GetFilePath())
+				case "StaticFS":
+					groupRouter.StaticFS(route.GetURL(), http.Dir(route.GetFilePath()))
+				default:
+					groupRouter.StaticFile(route.GetURL(), route.GetFilePath())
+				}
+			} else {
+				if route.GetMethod() == "Any" {
+					groupRouter.Any(route.GetURL(), route.ExecHandler()...)
+				} else {
+					groupRouter.Handle(route.GetMethod(), route.GetURL(), route.ExecHandler()...)
+				}
+			}
+		}
+	}
+}
+
+// gracefulShutdown 优雅关停服务
+func gracefulShutdown(r *gin.Engine) {
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", Settings.Port),
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	ctx1, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx1); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+	log.Println("Server exiting")
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"github.com/uuk020/gin-web-template/cmd/app/middlewares"
+	"github.com/uuk020/gin-web-template/cmd/app/router"
 	"github.com/uuk020/gin-web-template/cmd/app/utils"
 	"github.com/uuk020/gin-web-template/initialize"
 )
@@ -13,11 +14,9 @@ func main() {
 	initialize.Logger()
 	// 初始化数据库相关
 	initialize.InitMysql()
-	initialize.InitRedis()
-	// 初始化路由并且启动 gin 服务
-	r := initialize.Router()
-	err := r.Run(fmt.Sprintf(":%d", utils.Settings.Port))
-	if err != nil {
-		panic("gin 服务运行失败: " + err.Error())
-	}
+	//initialize.InitRedis()
+	// 初始化路由
+	router.App()
+	// 启动 gin 服务
+	utils.StartGinServ(true, middlewares.HttpRequestLogger(), middlewares.ServerDowntimeLogger(true))
 }
